02fc82a4f248072b5708b7e47c6aafe3d8fbdc7b
Ringard Raphael <raphael@admins-MacBook-Pro.local>
2024-02-12 14:54:48 +0100

    Implement navigation compenent

---
+++ src/components/navigation/navigation.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import './navigation.css'
+
+
+export default function Nav() {
+	return (
+		<nav className="nav-header">
+			<Link to="/" className="nav-header_link-home">
+				Accueil
+			</Link>
+			<Link to="/about" className="nav-header_link-about">
+				A Propos
+			</Link>
+		</nav>
+	);
+}

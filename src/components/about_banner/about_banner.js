98dc1173251714baa2e084b35e8de267246e4462
Ringard Raphael <raphael@admins-MacBook-Pro.local>
2024-02-12 14:50:26 +0100

    Implement about banner component

---
+++ src/components/about_banner/about_banner.js
@@ -0,0 +1,7 @@
+import './about_banner.css'
+
+//la bannière about est une image et un calque assombrissant qui varient selon le responsive
+
+export default function AboutBanner() {
+	return <div className="aboutBackground wh-msk"></div>;
+}

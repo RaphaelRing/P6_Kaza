f8c9a848fc8c7ad35108cc427687a4f6ccddcc9a
Ringard Raphael <raphael@admins-MacBook-Pro.local>
2024-02-12 15:02:33 +0100

    Implement error page

---
+++ src/pages/page_error/page_error.js
@@ -0,0 +1,14 @@
+import ErrorPage from "../../components/page_error/page_error";
+import Header from "../../components/header/header";
+import Footer from "../../components/footer/footer";
+
+export default function Error() {
+	return (
+		<>
+		<Header />
+			<ErrorPage />
+		<Footer />
+	
+		</>
+	);
+}
